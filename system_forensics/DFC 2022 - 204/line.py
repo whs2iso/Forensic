869b7658f21b8c4faf19ce07c87fe50bf99143bf
01star01ek <01star01ek@gmail.com>
2024-06-14 00:30:56 +0900

    Revert "Merge branch 'main' into Ryunayeon"
    This reverts commit 56902ceb4ff03fbd8374ead02906e64053d4b6c3, reversing
    changes made to 1fa9e8c3aab9704c9f468fe69a308adf480241e6.

--- system_forensics/DFC 2022 - 204/line.py
+++
@@ -1,8 +0,0 @@
-# filter_empty_lines.py
-input_file = 'usbdata1.txt'
-output_file = 'filtered_capdata.txt'
- 
-with open(input_file, 'r') as infile, open(output_file, 'w') as outfile:
-    for line in infile:
-        if line.strip():  # 공백이 아닌 줄만 필터링
-            outfile.write(line)
